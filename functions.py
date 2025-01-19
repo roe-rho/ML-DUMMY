7fe81822daee5d9c20b497a2efd6967bd61ca525
roe-rho <ronan472@gmail.com>
2025-01-19 03:16:23 +0100

    removed super linter and not needed imports

--- functions.py
+++ functions.py
@@ -1,8 +1,7 @@
 import os
-import tensorflow as tf
-from tensorflow.keras import layers, models
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.utils import to_categorical
+from tensorflow import layers, models
+from tensorflow import cifar10
+from tensorflow import to_categorical
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
