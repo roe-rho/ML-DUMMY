89ac75ac231b71deacc18638ddf902f513e229ab
roe-rho <ronan472@gmail.com>
2025-01-20 15:18:58 +0100

    128 filters

--- functions.py
+++ functions.py
@@ -34,11 +34,11 @@ def create_cnn(input_shape=(32, 32, 3), num_classes=10):
         model = models.Sequential([
             layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
             layers.MaxPooling2D((2, 2)),
-            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.Conv2D(64+64, (3, 3), activation='relu'),
             layers.MaxPooling2D((2, 2)),
-            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.Conv2D(64+64, (3, 3), activation='relu'),
             layers.Flatten(),
-            layers.Dense(64, activation='relu'),
+            layers.Dense(64+64, activation='relu'),
             layers.Dense(num_classes, activation='softmax')
         ])
         logger.info("CNN model created.")
